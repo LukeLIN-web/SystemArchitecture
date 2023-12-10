9b52efb1dd3ede1673ef73d82bbf718e26e09967
Luke <1263810658@qq.com>
2023-12-10 23:22:56 +0300

    whole system finished

--- exam/exam3/Simulator.py
+++ exam/exam3/Simulator.py
@@ -4,7 +4,7 @@ import random
 @dataclass
 class SimulatorConfigure:
     max_cycles = 100000
-    instruction_interval = 100
+    # instruction_interval = 100
 
     pipelinestall_ratio = 0.2
     data_access_ratio = 0.3
@@ -27,6 +27,7 @@ class SimulatorConfigure:
     max_bus_utilization = 0.8
     bus_speed = 6 * 10**9  # 6 GHz
     cache_line_size = 64  # Assuming 64 bytes cache line
+    chips_per_system = 5  # Example value
 
 class BusManager:
     def __init__(self):
@@ -34,63 +35,43 @@ class BusManager:
         self.queue = []
         self.processing_time = -1
         self.busy_time = 0
-        self.current_id = -1
+        self.current_id = None
 
-    def request_bus(self, core_id,access_time):
+    def request_bus(self, chipid, core_id,access_time):
         if self.busy:
-            self.queue.append((core_id,access_time))
+            self.queue.append((chipid, core_id,access_time))
         else:
             self.busy = True
             self.processing_time = access_time
-            self.current_id = core_id
+            self.current_id = [chipid, core_id]
 
     def release_bus(self):
         self.busy = False
         if self.queue:
-            core_id, access_time = self.queue.pop(0)
-            self.request_bus(core_id, access_time)
+            chipid, core_id, access_time = self.queue.pop(0)
+            self.request_bus(chipid, core_id, access_time)
         return self.current_id
 
     def process_cycle(self):
-        print(self.busy, self.queue, self.processing_time)
         if self.busy:
             self.busy_time += 1
             self.processing_time -=1
             if self.processing_time == 0:
                 self.release_bus()
-
-class Chip:
-    def __init__(self, config: SimulatorConfigure):
-        self.config = config
-        self.total_time = 0
-        self.bus_manager = BusManager()
-        self.cores = [Core(core_id, config, self.bus_manager) for core_id in range(config.cores_per_chip)]
-
-    def start(self):
-        for cycle in range(self.config.max_cycles):
-            self.total_time += 1  # Each core contributes one cycle
-            coreid = self.bus_manager.process_cycle() # receive from busmanager
-            if coreid is not None:
-                self.cores[coreid].waiting_for_memory = False
-            for core in self.cores:
-                core.process_cycle()
-        print("Total time:", self.bus_manager.busy_time)
-
-        average_bus_utilization = self.bus_manager.busy_time / self.total_time
-        print("Average bus utilization:", average_bus_utilization)
-
 class Core:
-    def __init__(self, core_id, config: SimulatorConfigure, bus_manager: BusManager):
+    def __init__(self, core_id, chip_id, config: SimulatorConfigure, bus_manager: BusManager):
         self.core_id = core_id
+        self.chip_id = chip_id
         self.config = config
         self.bus_manager = bus_manager
         self.waiting_for_memory = False
+
 
     def process_cycle(self):
         if self.waiting_for_memory:
             return  # Currently waiting for memory access
 
-        if random.random() < 0.1:  # 10% chance to issue an instruction
+        if random.random() < 0.3:  # 10% chance to issue an instruction
             if random.random() > self.config.L1i_hit_ratio and random.random() > self.config.L2_hit_ratio:
                 # L1 and L2 miss, need memory access
                 self.waiting_for_memory = True
@@ -98,9 +79,40 @@ class Core:
                     access_time = self.config.memory_access
                 else:
                     access_time = self.config.memory_access  * self.config.page_table_level # page walk
-                self.bus_manager.request_bus(self.core_id, access_time)
+                self.bus_manager.request_bus(self.chip_id, self.core_id, access_time)
+
+class Chip:
+    def __init__(self, chip_id, config: SimulatorConfigure, bus_manager: BusManager):
+        self.chip_id = chip_id
+        self.config = config
+        self.bus_manager = bus_manager
+        self.cores = [Core(core_id, self.chip_id, config, self.bus_manager) for core_id in range(config.cores_per_chip)]
+
+    def process_cycle(self):
+        for core in self.cores:
+            core.process_cycle()
+
+
+class System:
+    def __init__(self, config: SimulatorConfigure):
+        self.config = config
+        self.bus_manager = BusManager()
+        self.chips = [Chip(chip_id, config, self.bus_manager) for chip_id in range(config.chips_per_system)]
+
+    def start(self):
+        for cycle in range(self.config.max_cycles):
+            id = self.bus_manager.process_cycle() # receive from busmanager
+            if id is not None:
+                chipid, coreid = id
+                self.chips[chipid].cores[coreid].waiting_for_memory = False
+            for chip in self.chips:
+                chip.process_cycle()
+        print("Total time:", self.bus_manager.busy_time)
+        average_bus_utilization = self.bus_manager.busy_time / self.config.max_cycles
+        print("Average bus utilization:", average_bus_utilization)
+
 
 if __name__ == '__main__':
     config = SimulatorConfigure()
-    simulator = Chip(config)
-    simulator.start()
+    system = System(config)
+    system.start()
